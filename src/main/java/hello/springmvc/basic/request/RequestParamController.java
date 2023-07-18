c2130700642d746f840e96f50ddacb76d956018e
Won_Jin <foryour_0827@naver.com>
2023-07-18 12:43:18 +0900

    23.07.18 * GET 방식과 POST 방식(메시지바디전달)로 쿼리파라미터 정보를 알아내보았다

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    // GET, POST 구분 없이 HTTP 요청 데이터를 객체화 해서 사용가능
+    // 쿼리 파라미터의 정보를 가공하는 것에 의의를 둔다.
+    
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username = {}, age = {}", username, age);
+
+        response.getWriter().write("ok");
+    }
+}
