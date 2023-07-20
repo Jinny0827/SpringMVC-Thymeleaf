3af42bf2fe82c8c8ffd3dfbca53dcbdcc42c1aa7
Won_Jin <foryour_0827@naver.com>
2023-07-21 00:36:22 +0900

    23.07.21 * JSON 타입으로 HTTP 메시지 바디로 직접 응답하는 경우 1. ResponseEntity<객체>를 반환타입으로 사용하여 객체에 데이터를 set 후 ResponseEntity<>(생성객체, HTTP상태코드); 를 응답
    2. @ResponseBody 어노테이션을 사용하고 반환타입을 객체로 적는다.
    똑같이 객체에 데이터를 set 후 객체 자체를 반환
    -> 이 과정에서 HTTP 메시지 컨버터가 변환 후 반환해준다.
    -> @ResponseBody를 사용하면 HTTP 상태 코드를 반환해주기 힘든데
        -> 이럴때 @ResponseStatus(HttpStatus.상태) 를 적어준다.
           -> 정적으로 상태코드를 반환할때만 사용하고
           -> 동적으로 상태코드를 반환시엔 ResponseEntity<>를 사용한다.
              -> if로 상태코드별 반환 return을 하면 될듯싶다...

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -1,11 +1,13 @@
 package hello.springmvc.basic.response;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -45,6 +47,36 @@ public class ResponseBodyController {
 
     // 이제는 JSON 타입으로 HTTP 메시지 바디에 내용을
     // 직접 전달하는 방식이다.
+
+    // 반환타입을 ResponseEntity를 사용하고 제네릭변수를 HelloData(DTO)
+    // 데이터를 직접 담아서 응답해준다.
+    // 반환시 HttpStatus로 응답 상태를 보내줄 수 있다.
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("원진");
+        helloData.setAge(29);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    // @ResponseBody 어노테이션을 사용하여
+    // 반환타입에 HelloData를 넣고
+    // HelloData의 set으로 데이터 입혀서
+    // helloData 객체를 직접 반환
+    // HttpStatus(상태)는 어떻게 보낼까?
+    // @ResponseStatus(HttpStatus.~~) 를 사용하면 된다.
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("원진");
+        helloData.setAge(29);
+
+        return helloData;
+    }
+
 
 
 }
